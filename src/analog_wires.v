b8e023a5afe1bea97d62e08fa808428d0c052a1e
Christoph Maier <christoph.maier@ieee.org>
2026-02-13 01:53:24 +0100

    gds/analog_wires.lef src/analog_wires.v : hand edited simple stuff

---
+++ src/analog_wires.v
@@ -0,0 +1,12 @@
+// File: analog_wires.v
+// analog wires (etc.) digital placeholder
+// Yosys hints: blackbox + keep_hierarchy.
+(* blackbox *)
+module analog_wires (
+    inout Iout,
+    inout [1:0] VcascP,
+//    inout [1:0] VbiasP,
+    inout i_in,
+    inout i_out
+);
+endmodule
