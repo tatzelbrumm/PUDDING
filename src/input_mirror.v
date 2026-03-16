b88effdcd695ed04e9d39bab6b369ad954706859
Christoph Maier <christoph.maier@ieee.org>
2026-03-16 15:20:36 +0100

    gds/input_mirror.lef src/input_mirror.v : change LEF by hand, clean up Verilog dummy

--- src/input_mirror.v
+++ src/input_mirror.v
@@ -13,17 +13,17 @@ module input_mirror (
 
 //  wire bias;
 //  wire casc;
-    wire vdda;
-    wire vssa;
+//  wire vdda;
+//  wire vssa;
 
 //  assign VbiasP[0] = bias;
 //  assign VbiasP[1] = bias;
 //  assign VcascP[0] = casc;
 //  assign VcascP[1] = casc;
-    assign VDDA[3] = vdda;
-    assign VDDA[2] = vdda;
-    assign VDDA[1] = vdda;
-    assign VDDA[0] = vdda;
-    assign VSSA[1] = vssa;
-    assign VSSA[0] = vssa;
+//  assign VDDA[3] = vdda;
+//  assign VDDA[2] = vdda;
+//  assign VDDA[1] = vdda;
+//  assign VDDA[0] = vdda;
+//  assign VSSA[1] = vssa;
+//  assign VSSA[0] = vssa;
 endmodule
